9ea7b2a05cc7b64d9502fd4f131fa076d7fd9b58
Manjeet Thadani <mthandani@grepruby.com>
2018-09-13 16:16:51 +0530

    Support for bitcoin testnet

--- lib/bip44coins/bip44coins.go
+++ lib/bip44coins/bip44coins.go
@@ -2,5 +2,6 @@ package bip44coins
 
 const (
 	Bitcoin uint16 = 0
+	TestNet uint16 = 1
 	Ether   uint16 = 60
 )
--- lib/decoder.go
+++ lib/decoder.go
@@ -7,45 +7,12 @@ import (
 	"gitlab.com/arout/Vault/lib/bip44coins"
 )
 
-// IRawTx Raw transaction interface
-// to enable decoding of all variants of raw transactions (JSON)
-type IRawTx interface{}
-
-// EthereumRawTx Ethereum raw transaction implements IRawTx
-// to store raw Ethereum JSON payload
-type EthereumRawTx struct {
-	Nonce    uint64 `json:"nonce"`
-	Value    uint64 `json:"value"`
-	GasLimit uint64 `json:"gasLimit"`
-	GasPrice uint64 `json:"gasPrice"`
-	To       string `json:"to"`
-	Data     string `json:"data"`
-	ChainID  int64  `json:"chainId"`
-	IRawTx
-}
-
-type UTXO struct {
-	TxHash string `json:"txhash"`
-	Vout   uint32 `json:"vout"`
-}
-
-type PayeeAddress struct {
-	Address string `json:"address"`
-	Amount  int64  `json:"amount"`
-}
-
-type BitcoinRawTx struct {
-	Inputs  []*UTXO         `json:"inputs"`
-	Outputs []*PayeeAddress `json:"outputs"`
-	IRawTx
-}
-
 // DecodeRawTransaction decodes input payload into suitable raw transaction
 // depending on cointype provided
 // returns error if no supported coin type found
 func DecodeRawTransaction(coinType uint16, payload string) (IRawTx, error) {
 	switch coinType {
-	case bip44coins.Bitcoin:
+	case bip44coins.Bitcoin, bip44coins.TestNet:
 		var tx BitcoinRawTx
 		err := json.Unmarshal([]byte(payload), &tx)
 		return tx, err
