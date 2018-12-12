5b3c2ebe32a04daff49a4d8b96e5f286b62261c2
akhil <akhil@grepruby.com>
2018-12-12 12:28:27 +0530

    fixed seed value to not be 0x00 everytime

--- api/path_address.go
+++ api/path_address.go
@@ -18,11 +18,11 @@ import (
 func (b *backend) pathAddress(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
 	backendLogger := b.Backend.Logger()
 	if err := helpers.ValidateFields(req, d); err != nil {
-		logger.Log(backendLogger, config.Error, "1. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
-	// UUID of user which want to sign transaction
+	// UUID of user required to sign transaction
 	uuid := d.Get("uuid").(string)
 
 	// derivation path
@@ -36,11 +36,11 @@ func (b *backend) pathAddress(ctx context.Context, req *logical.Request, d *fram
 		derivationPath = config.BitsharesDerivationPath
 	}
 
-	logger.Log(backendLogger, config.Info, "2. address:", fmt.Sprintf("request path=[%v] cointype=%v ", derivationPath, coinType))
+	logger.Log(backendLogger, config.Info, "address:", fmt.Sprintf("request path=[%v] cointype=%v ", derivationPath, coinType))
 
 	// validate data provided
 	if err := helpers.ValidateData(ctx, req, uuid, derivationPath); err != nil {
-		logger.Log(backendLogger, config.Error, "3. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
@@ -48,50 +48,55 @@ func (b *backend) pathAddress(ctx context.Context, req *logical.Request, d *fram
 	path := config.StorageBasePath + uuid
 	entry, err := req.Storage.Get(ctx, path)
 	if err != nil {
-		logger.Log(backendLogger, config.Error, "4. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
-	// obtain mnemonic, passphrase of user
+	// obtain mnemonic and passphrase of user
 	var userInfo helpers.User
 	err = entry.DecodeJSON(&userInfo)
 	if err != nil {
-		logger.Log(backendLogger, config.Error, "5. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
 	// obtain seed from mnemonic and passphrase
+	logger.Log(backendLogger, config.Info, "mnemonic", userInfo.Mnemonic, userInfo.Passphrase)
 	seed, err := lib.SeedFromMnemonic(userInfo.Mnemonic, userInfo.Passphrase)
+
+	logger.Log(backendLogger, config.Info, "dp", derivationPath)
 
 	// obtains blockchain adapater based on coinType
 	adapter, err := adapter.GetAdapter(uint16(coinType), seed, derivationPath)
 	if err != nil {
-		logger.Log(backendLogger, config.Error, "6. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
 	// Generates and stores ECDSA private key in adapter
-	_, err = adapter.DerivePrivateKey(backendLogger)
+	priv, err := adapter.DerivePrivateKey(backendLogger)
 	if err != nil {
-		logger.Log(backendLogger, config.Error, "7. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
+	logger.Log(backendLogger, config.Info, "priv", priv)
+
 	pubKey, err := adapter.DerivePublicKey(backendLogger)
 	if err != nil {
-		logger.Log(backendLogger, config.Error, "8. address:", err.Error(), "which state", pubKey)
+		logger.Log(backendLogger, config.Error, "address:", err.Error(), "which state", pubKey)
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
 	address, err := adapter.DeriveAddress(backendLogger)
 	if err != nil {
-		logger.Log(backendLogger, config.Error, "9. address:", err.Error())
+		logger.Log(backendLogger, config.Error, "address:", err.Error())
 		return nil, logical.CodedError(http.StatusUnprocessableEntity, err.Error())
 	}
 
 	logger.Log(backendLogger, config.Info, "address:", fmt.Sprintf("\n[INFO ] address:  derived publicKey=[%v], address=[%v]", pubKey, address))
 
-	// Returns publicKey, address as output
+	// Returns publicKey and address as output
 	return &logical.Response{
 		Data: map[string]interface{}{
 			"uuid":      uuid,
--- lib/adapter/bitshares_adapter.go
+++ lib/adapter/bitshares_adapter.go
@@ -7,6 +7,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
@@ -36,7 +37,7 @@ type BitsharesAdapter struct {
 // sets seed, derivation path as internal data
 func NewBitsharesAdapter(seed []byte, derivationPath string, isDev bool) *BitsharesAdapter {
 	adapter := new(BitsharesAdapter)
-	adapter.Seed = make([]byte, 64)
+	adapter.Seed = seed
 	adapter.DerivationPath = config.BitsharesDerivationPath
 	adapter.IsDev = isDev
 	adapter.zeroAddress = "0x0000000000000000000000000000000000000000"
@@ -44,8 +45,7 @@ func NewBitsharesAdapter(seed []byte, derivationPath string, isDev bool) *Bitsha
 	return adapter
 }
 
-// DerivePrivateKey Derives derivation path to obtain private key
-// checks for errors
+// DerivePrivateKey Derives Private Key from mnemonic coressponding to a hard coded derivation path i.e "m/44'/69'/69'/69/69" for bitshares, to obtain private key and checks for errors.
 func (e *BitsharesAdapter) DerivePrivateKey(backendLogger log.Logger) (string, error) {
 	// obatin private key from seed + derivation path
 	btcecPrivKey, err := lib.DerivePrivateKey(e.Seed, e.DerivationPath, e.IsDev)
@@ -91,14 +91,42 @@ func (e *BitsharesAdapter) DerivePublicKey(logger log.Logger) (string, error) {
 	return publicKey, nil
 }
 
-// DeriveAddress Address in Bitsahres can be avoided for most cases by using account names instead.
-// TODO : ADD a proper address generation logic.
+// DeriveAddress Address in Bitsahres can be avoided for most cases by using account names instead. However it is needed for "nathan"
 func (e *BitsharesAdapter) DeriveAddress(logger log.Logger) (string, error) {
-	return "", nil // returns null for address
+
+	// obatin private key from seed + derivation path
+	_, err := e.DerivePrivateKey(logger)
+	if err != nil {
+		return "", err
+	}
+
+	// obtain wif object from private key structure.
+	wif, err := btcutil.DecodeWIF(e.PrivateKey)
+	if err != nil {
+		return "", err
+	}
+
+	pubKeyBytes := wif.PrivKey.PubKey().SerializeCompressed()
+
+	shaHash := sha512.New()
+	shaHash.Write(pubKeyBytes)
+	pubKey1 := shaHash.Sum(nil)
+
+	mdHash := ripemd160.New()
+	mdHash.Write(pubKey1)
+	rawAddress := mdHash.Sum(nil)
+
+	// Checksum generation. Checksum is ripemd hash of the address
+	mdHash = ripemd160.New()
+	mdHash.Write(rawAddress)
+	checkSum := mdHash.Sum(nil)
+	address := append(rawAddress, checkSum[0:4]...)
+
+	result := "BTS" + base58.Encode(address)
+	return result, nil // returns null for address
 }
 
-// GetBlockchainNetwork returns network config
-// default isDev=false i.e. Mainnet
+// GetBlockchainNetwork returns network config. Default isDev=false i.e. Mainnet for Bitshares as it is not needed for BTS.
 func (e *BitsharesAdapter) GetBlockchainNetwork() string {
 	if e.IsDev {
 		return "testnet"
@@ -107,7 +135,6 @@ func (e *BitsharesAdapter) GetBlockchainNetwork() string {
 }
 
 // CreateSignedTransaction creates and signs raw transaction from transaction digest + private key
-// TODO :- INCLUDE BTS TRANSACTION DIGEST SIGNATURE LOGIC.
 func (e *BitsharesAdapter) CreateSignedTransaction(payload lib.IRawTx, backendLogger log.Logger) (string, error) {
 	if _, err := e.DerivePrivateKey(backendLogger); err != nil {
 		return "", err
@@ -117,7 +144,6 @@ func (e *BitsharesAdapter) CreateSignedTransaction(payload lib.IRawTx, backendLo
 	wifs[0] = e.PrivateKey
 	logger.Log(backendLogger, config.Info, "privateWIF:", wifs[0])
 
-	// creates raw transaction from payload
 	digestString, err := e.createRawTransaction(payload, backendLogger)
 	if err != nil {
 		logger.Log(backendLogger, config.Error, "signature:", err.Error())
@@ -135,8 +161,7 @@ func (e *BitsharesAdapter) CreateSignedTransaction(payload lib.IRawTx, backendLo
 	return signature[0], err
 }
 
-// generates raw transaction from payload
-// returns raw transaction + chainId + error (if any)
+// creates raw transaction from payload which in this case is of the form {"transactionDigest":"..someValue.."}
 func (e *BitsharesAdapter) createRawTransaction(p lib.IRawTx, backendLogger log.Logger) (string, error) {
 	data, _ := json.Marshal(p)
 	var payload lib.BitsharesRawTx
@@ -160,8 +185,7 @@ func (e *BitsharesAdapter) createRawTransaction(p lib.IRawTx, backendLogger log.
 	return payload.TransactionDigest, nil
 }
 
-// validate payload inputs and returns type of
-// transaction if payload is valid
+// validates payload TODO : check for a valid hex string.
 func validateBTSPayload(payload lib.BitsharesRawTx) bool {
 	// _, err := strconv.ParseUint(payload.TransactionDigest, 16, 64)
 	// if err != nil {
@@ -275,10 +299,10 @@ func recoverKeyFromSignature(curve *secp256k1.KoblitzCurve, sig *secp256k1.Signa
 func toBytes(pub ecdsa.PublicKey) []byte {
 	x := pub.X.Bytes()
 
-	/* Pad X to 32-bytes */
+	// Pad X to 32-bytes
 	paddedX := append(bytes.Repeat([]byte{0x00}, 32-len(x)), x...)
 
-	/* Add prefix 0x02 or 0x03 depending on ylsb */
+	// Add prefix 0x02 or 0x03 depending on ylsb
 	if pub.Y.Bit(0) == 0 {
 		return append([]byte{0x02}, paddedX...)
 	}
@@ -292,13 +316,10 @@ func signBufferSha256(bufSha256 []byte, privateKey *ecdsa.PrivateKey) []byte {
 	copy(bufSha256Clone, bufSha256)
 
 	nonce := 0
-	// int(time.Now().Unix()
 	for {
-		// r, s, err := rfc6979.
 		r, s, err := rfc6979.SignECDSA(privateKey, bufSha256Clone, sha256.New, nonce)
 		nonce++
 		if err != nil {
-			// log.Println(err)
 			return nil
 		}
 
@@ -316,12 +337,9 @@ func signBufferSha256(bufSha256 []byte, privateKey *ecdsa.PrivateKey) []byte {
 			curve := secp256k1.S256()
 			maxCounter := 4 //maxCounter := (curve.H+1)*2
 			for i := 0; i < maxCounter; i++ {
-				//for i := 0; i < (curve.H+1)*2; i++ {
-				//for i := 0; ;i++ {
 				pk, err := recoverKeyFromSignature(curve, ecsignature, bufSha256Clone, i, true)
 
 				if err == nil && pk.X.Cmp(key.X) == 0 && pk.Y.Cmp(key.Y) == 0 {
-					//result := make([]byte, 1, 2*curve.byteSize+1)
 					byteSize := curve.BitSize / 8
 					result := make([]byte, 1, 2*byteSize+1)
 					result[0] = 27 + byte(i)
@@ -356,7 +374,7 @@ func signBufferSha256(bufSha256 []byte, privateKey *ecdsa.PrivateKey) []byte {
 func getSignature(transactionDigest []byte, wifs []string) []string {
 	privKeys := make([]*secp256k1.PrivateKey, len(wifs))
 	for index, wif := range wifs {
-		w, err := btcutil.DecodeWIF(wif) // check it
+		w, err := btcutil.DecodeWIF(wif)
 		if err != nil {
 			panic(err)
 		}
